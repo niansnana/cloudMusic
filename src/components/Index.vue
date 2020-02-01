6accdd3b2fad93f52a5e589b6add934ec827bc69
niansnana <niansnana@gmail.com>
2020-02-01 17:05:00 +0800

    statr

---
+++ src/components/Index.vue
@@ -0,0 +1,20 @@
+<template>
+  <el-container>
+    <el-header>header</el-header>
+    <el-main>main</el-main>
+    <el-footer>footer</el-footer>
+  </el-container>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="less" scoped>
+.el-container {
+  height: 100%;
+  .el-main {
+    background-color: #eaedf1;
+  }
+}
+</style>
